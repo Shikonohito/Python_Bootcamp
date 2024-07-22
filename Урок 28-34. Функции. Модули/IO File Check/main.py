056a335f3447306b87979faec5db73a389f6fb35
Shikonohito <shikonohito@gmail.com>
2024-07-22 12:19:10 +0400

    Updated IO File Check - now program saves output in file.

--- Урок 28-34. Функции. Модули/IO File Check/main.py
+++ Урок 28-34. Функции. Модули/IO File Check/main.py
@@ -1,14 +1,14 @@
-import os.path
 from time import time
-from os import path, remove
+from os import remove
+from os.path import isfile, exists
 
 
 def timer(func):  # Декоратор
     def wrapper(*args, **kwargs):  # Обёртка
         start_time = time()
-        result = func(*args, **kwargs)
+        func(*args, **kwargs)
         end_time = time()
-        print(f"Execution time: {end_time - start_time} seconds. File size: {path.getsize(args[0])} Bytes. File name: {args[0]}.")
+        result = end_time - start_time
         return result
     return wrapper
 
@@ -44,24 +44,59 @@ def read_all(input_file_path, output_file_path):
         file.write(content)
 
 
+def init_tbl(func_names: tuple, file_path="time.txt"):
+    if isfile(file_path):
+        remove(file_path)
+    with open(file_path, "ab") as file_output:
+        file_output.write("data_type,".encode("utf-8"))
+        for i in range(len(func_names) - 1):
+            file_output.write(f"{func_names[i]},".encode("utf-8"))
+        file_output.write(f"{func_names[len(func_names) - 1]}".encode("utf-8"))
+
+
+def start_check_func_time(funcs: dict, files: tuple, result_out: dict):
+    for file_name in files:
+        result_dict[file_name] = []
+        for func_read in funcs.values():
+            input_fp = f"Text files\\{file_name}"
+            output_fp = f"Text files\\Output {file_name}"
+            result_time = func_read(input_fp, output_fp)
+            result_out[file_name].append(result_time)
+            if exists(output_fp):
+                remove(output_fp)
+
+
+def write_to_txt(some_str: str, file_path="time.txt"):
+    with open(file_path, "ab") as file_output:
+        file_output.write(some_str.encode("utf-8"))
+
+
+def dict_to_str(some_dict: dict):
+    result = ""
+    for key, item_list in some_dict.items():
+        result += f"\n{key}," + ",".join(map(str, item_list))
+    return result
+
+
+WRITE_PATH = "time.txt"
+
 read_funcs = {"read_block(1)": read_block(1),
               "read_line": read_line,
               "read_block(8192)": read_block(8192),
               "read_all": read_all}
 
-files = ["8KB.txt", "16KB.txt", "32KB.txt",
-         "64KB.txt", "Newlines64KB.txt",
-         "100KB.txt", "Newlines100KB.txt",
-         "1MB.txt", "Newlines1MB.txt",
-         "10MB.txt", "Newlines10MB.txt",
-         "100MB.txt", "Newlines100MB.txt"]
+files_txt = ("8KB.txt", "16KB.txt", "32KB.txt",
+             "64KB.txt", "Newlines64KB.txt",
+             "100KB.txt", "Newlines100KB.txt",
+             "1MB.txt", "Newlines1MB.txt",
+             "10MB.txt", "Newlines10MB.txt",
+             "100MB.txt", "Newlines100MB.txt")
 
-for func_name, func in read_funcs.items():
-    print(func_name)
-    for file_name in files:
-        input_file_path = f"Text files\\{file_name}"
-        output_file_path = f"Text files\\Output {file_name}"
-        func(input_file_path, output_file_path)
-        if os.path.exists(output_file_path):
-            remove(output_file_path)
-    print()
+result_dict = dict()
+
+start_check_func_time(read_funcs, files_txt, result_dict)
+init_tbl(tuple(read_funcs.keys()), WRITE_PATH)
+key_value_str = dict_to_str(result_dict)
+write_to_txt(key_value_str, WRITE_PATH)
+
+
