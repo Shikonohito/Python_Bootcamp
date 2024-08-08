9ec9cbe3611838d1b597793c07e59f3df11316b3
Shikonohito <shikonohito@gmail.com>
2024-08-08 11:10:53 +0400

    Updated HW38 - fixed remarks.

--- Programs/Grade/grade.py
+++ Programs/Grade/grade.py
@@ -4,4 +4,3 @@ result_grade = 0
 for current_task in range(1, num_tasks + 1):
     result_grade += int(input(f"Задание {current_task}. Оценка от 1 до {grade_total}: "))
 print(f"Оценка {result_grade / num_tasks}")
-
